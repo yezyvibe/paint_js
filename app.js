1d1731ad0f8ddcd731f421ad32aea0c34d1b0ec0
yezyvibe <yeji0501@gmail.com>
2021-12-31 11:36:06 +0900

    feat: handle brush range change

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
-
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 // canvas.width = 700;
 // canvas.height = 700;
 
@@ -12,6 +13,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -39,6 +41,21 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill"
+  } else {
+    filling = true;
+    mode.innerText = "Paint"
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -47,3 +64,11 @@ if (canvas) {
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+} 
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
